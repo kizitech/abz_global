bc70c033c4bf57d96d1cc38d7e00a877b2ac96b9
Ohani Kizito <kizitoohani@gmail.com>
2025-01-28 11:13:58 +0100

    footer $ Testimonial

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -6,7 +6,7 @@ const Testimonials = () => {
       <h2 className="text-center text-xl font-light my-10 lg:my-20 tracking-wide text-gray-800">What People are saying</h2>
       <div className="flex flex-wrap justify-center">
         {testimonials.map((testimonial, index) => (
-          <div key={index} className="w-full sm:w-1/2 lg:w-1/3 py-2">
+          <div key={index} className="w-full sm:w-1/2 lg:w-1/3 sm:px-2 py-2">
             <div className="bg-neutral-300 rounded-md p-6 text-md border border-neutral-800 font-thin">
               <p>{testimonial.text}</p>
               <div className="flex mt-8 items-start">
