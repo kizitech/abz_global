fadb0489bc0182d8c66b4a94defd0dd6908a91bc
Ohani Kizito <125478867+kizitech@users.noreply.github.com>
2025-01-31 12:36:02 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { resourcesLinks, platformLinks, communityLinks } from "../constants";
 const Footer = () => {
   return (
     <footer className="mt-20 border-t py-10 px-6 border-neutral-700 bg-gray-100">
-      <div className="grid grid-cols-2 lg:grid-cols-3 gap-10">
+      <div className="grid grid-cols-2 lg:grid-cols-3 gap-10 max-w-7xl mx-auto">
         <div>
           <h3 className="text-md font-semibold mb-4 text-gray-800">Resources</h3>
           <ul className="space-y-2">
